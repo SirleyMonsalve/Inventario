87799985f88f3ea3e59a0c7676c5bcf4f27010c6
sirleyMonsalve <denis.sirleymp@gmail.com>
2020-09-17 18:51:34 -0500

    Proyecto inicial

---
+++ src/main/java/co/com/Inventarioproducto/InventarioProductoApplication.java
@@ -0,0 +1,13 @@
+package co.com.Inventarioproducto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioProductoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioProductoApplication.class, args);
+	}
+
+}
